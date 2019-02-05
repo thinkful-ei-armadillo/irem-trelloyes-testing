7215acd8f94d960d67cad5c3cba19d946c78c818
Irem Sen <secilreel@yahoo.com>
2019-02-05 12:15:42 -0500

    changing the file structure

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import STORE from './store';
-
-ReactDOM.render(<App store={STORE}/>, document.querySelector('#root'));
